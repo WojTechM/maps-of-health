1cab2b39282b279d9cc18edd043d90a0266bc357
WojTechM <wojciech.m1997@gmail.com>
2018-11-24 16:02:11 +0100

    Update annotations in Hospital class

--- src/main/java/pl/healthmaps/registration/model/Hospital.java
+++ src/main/java/pl/healthmaps/registration/model/Hospital.java
@@ -1,11 +1,17 @@
 package pl.healthmaps.registration.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Hospital {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
     private String name;
     private String description;
 
