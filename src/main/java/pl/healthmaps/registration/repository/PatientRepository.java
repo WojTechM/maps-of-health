fc7eee7df57aa8fcda474777dfafb1fe0e304960
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2018-11-24 15:55:46 +0100

    Add PatientRepository

---
+++ src/main/java/pl/healthmaps/registration/repository/PatientRepository.java
@@ -0,0 +1,11 @@
+package pl.healthmaps.registration.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.healthmaps.registration.model.Patient;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Optional<Patient> findByPesel(long pesel);
+}
