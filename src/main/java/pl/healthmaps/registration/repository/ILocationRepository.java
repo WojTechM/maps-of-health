ec1ebfb88a5a92095e14774ad54c551f6c4a45e1
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2018-11-24 16:57:52 +0100

    Fix typo

--- src/main/java/pl/healthmaps/registration/repository/ILocationRepository.java
+++ src/main/java/pl/healthmaps/registration/repository/ILocationRepository.java
@@ -3,5 +3,5 @@ package pl.healthmaps.registration.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.healthmaps.registration.model.Location;
 
-public interface LocationRepository extends JpaRepository<Location, Long> {
+public interface ILocationRepository extends JpaRepository<Location, Long> {
 }
