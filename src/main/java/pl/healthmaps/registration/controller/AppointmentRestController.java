42a64e349e723fb6a231a8d7012987bbffce97cc
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2018-11-24 17:16:21 +0100

    Add appointment controller

---
+++ src/main/java/pl/healthmaps/registration/controller/AppointmentRestController.java
@@ -0,0 +1,28 @@
+package pl.healthmaps.registration.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.healthmaps.registration.model.Appointment;
+import pl.healthmaps.registration.service.AppointmentService;
+
+@RestController
+@RequestMapping("/api/appointment")
+public class AppointmentRestController {
+
+    private AppointmentService appointmentService;
+
+    @Autowired
+    public AppointmentRestController(AppointmentService appointmentService) {
+        this.appointmentService = appointmentService;
+    }
+
+    @GetMapping(path = "/{id}")
+    ResponseEntity<Appointment> getAppointmentById(@PathVariable long id) {
+        return new ResponseEntity<>(this.appointmentService.findById(id), HttpStatus.OK);
+    }
+}
