9a78cc1df7506c2229cc9cede0d7e72a1ddd2395
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2018-11-24 16:55:54 +0100

    Add Appointment model

---
+++ src/main/java/pl/healthmaps/registration/model/Appointment.java
@@ -0,0 +1,34 @@
+package pl.healthmaps.registration.model;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
+    @OneToOne
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    private LocalDateTime start;
+    private LocalDateTime end;
+
+}
