0e4ae6cb73f8f9baf378d4c81ef18be281cd8744
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2018-11-24 15:55:13 +0100

    Implement Patient model

--- src/main/java/pl/healthmaps/registration/model/Patient.java
+++ src/main/java/pl/healthmaps/registration/model/Patient.java
@@ -1,4 +1,52 @@
 package pl.healthmaps.registration.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    private long pesel;
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getPesel() {
+        return this.pesel;
+    }
+
+    public void setPesel(long pesel) {
+        this.pesel = pesel;
+    }
 }
