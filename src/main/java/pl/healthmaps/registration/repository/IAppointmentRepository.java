02287082045ed0053434125de94a2ba85b2454f6
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2018-11-25 00:27:30 +0100

    Fix SQL query

--- src/main/java/pl/healthmaps/registration/repository/IAppointmentRepository.java
+++ src/main/java/pl/healthmaps/registration/repository/IAppointmentRepository.java
@@ -7,6 +7,6 @@ import pl.healthmaps.registration.model.Appointment;
 
 public interface IAppointmentRepository  extends JpaRepository<Appointment, Long> {
 
-    @Query(value = "SELECT * FROM appointment AS app INNER JOIN doctor ON app.id = doctor_id INNER JOIN hospital AS h ON h.id = doctor.id WHERE h.id = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM appointment as app INNER JOIN doctor ON app.doctor_id = doctor.id INNER JOIN hospital AS h ON h.id = doctor.hospital_id WHERE h.id = ?1", nativeQuery = true)
     List<Appointment> getAllByHospital(long id);
 }
