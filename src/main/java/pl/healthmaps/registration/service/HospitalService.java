b91acb7f6508107cc54498617d2410d970be6ae5
WojTechM <wojciech.m1997@gmail.com>
2018-11-24 15:52:10 +0100

    Implement Hospital side - returns sample data

--- src/main/java/pl/healthmaps/registration/service/HospitalService.java
+++ src/main/java/pl/healthmaps/registration/service/HospitalService.java
@@ -1,4 +1,21 @@
 package pl.healthmaps.registration.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.healthmaps.registration.model.Hospital;
+import pl.healthmaps.registration.repository.IHospitalRepository;
+
+@Service
 public class HospitalService {
+
+    private final IHospitalRepository repository;
+
+    @Autowired
+    public HospitalService(IHospitalRepository repository) {
+        this.repository = repository;
+    }
+
+    public Hospital getHospitalById(long id) {
+        return repository.findById(id).orElse(null);
+    }
 }
