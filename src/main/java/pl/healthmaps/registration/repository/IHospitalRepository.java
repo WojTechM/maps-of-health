17a0036b2eb9f09e437d6b6c4eb627dd462969b0
WojTechM <wojciech.m1997@gmail.com>
2018-11-25 10:35:46 +0100

    Update way of retrieving data from database

--- src/main/java/pl/healthmaps/registration/repository/IHospitalRepository.java
+++ src/main/java/pl/healthmaps/registration/repository/IHospitalRepository.java
@@ -11,6 +11,6 @@ import java.util.List;
 @Repository
 public interface IHospitalRepository extends JpaRepository<Hospital, Long> {
 
-    @Query(value = "select * from hospital as h left join location as l on (h.id = l.hospital_id) where l.longitude between (?1 -1) and (?2 + 1);", nativeQuery = true)
+    @Query(value = "select * from hospital as h left join location as l on (h.id = l.hospital_id) where l.latitude between (?1 -1) and (?1 + 1) and l.longitude between (?2 - 1) and (?2 + 1);", nativeQuery = true)
     public List<Hospital> findAllInSquare(double latitude, double longitude);
 }
